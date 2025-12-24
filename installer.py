1252e49c0d55ef925593484a5bba20e148e5aee8
ALexH256 <fengalex13@outlook.com>
2025-12-25 00:17:54 +0800

    并发下载功能

--- installer.py
+++ installer.py
@@ -2,6 +2,7 @@ import configparser
 import requests
 import os
 from urllib.parse import urlparse
+from tqdm import tqdm
 
 config = configparser.ConfigParser()
 config.read("config.ini")
@@ -9,17 +10,3 @@ config.read("config.ini")
 list_url = config["download"]["version_manifest"]
 root_path = config["path"]["root_path"]
 
-def download_file(url, save_dir):
-    response = requests.get(url)
-    response.raise_for_status()
-    parsed_url = urlparse(url)
-    filename = os.path.basename(parsed_url.path)
-    if not filename:
-        filename = "downloaded_file"
-    save_path = os.path.join(save_dir, filename)
-    os.makedirs(save_dir, exist_ok=True)
-    with open(save_path, 'wb') as f:
-        f.write(response.content)
-    print(f"Downloaded {filename} to {save_path}")
-
-download_file(list_url, root_path)
