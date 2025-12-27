0e4c29d09602b651d3cdc4fa6caa36ab97d90d03
ALexH256 <fengalex13@outlook.com>
2025-12-27 15:34:08 +0800

    添加简单的列出可下载版本功能

--- src/launchcore/installer.py
+++ src/launchcore/installer.py
@@ -1,4 +1,5 @@
 from .downloader import download_files as download_files, download_file as download_file
+import json
 
 def main(args, config) :
     url = config['download']['version_manifest']
@@ -11,6 +12,31 @@ def main(args, config) :
 def update(url, root) :
     out = download_file(url, root + '/versionlist/version_manifest.json', cache_dir = root + '/cache')
     if out[0] :
-        print("success")
+        print('success')
     else :
-        print("fail")
+        print('fail')
+
+def read_json(file: str) :
+    with open(file, 'r', encoding = 'utf-8') as f :
+        js = json.load(f)
+    return js
+
+def list(list_file, type) :
+    show = []
+    data = read_json(list_file)        
+    if type == 'nothing' :
+        for ver in data['versions'] :
+            show.append('{:<25} {:<15}'.format(ver['id'], ver['type']))
+
+    else :
+        for ver in data['versions'] :
+            if type == ver['type'] :
+                show.append('{:<25} {:<15}'.format(ver['id'], ver['type']))
+
+    if len(show) == 0 :
+        print(f'不存在类型: {type}')
+    else :
+        print("{:<25} {:<15}".format('version name', 'type'))
+        print("-" * 40)
+        for line in show :
+            print(line)
